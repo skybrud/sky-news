dedafd39c2b67b08ed6fd2a0d9775e8fa489eb7b
Stefan Schonert <sh@schonert.dk>
2016-09-22 15:01:29 +0200

    clean up + match sky-list

--- sky-news.directive.ts
+++ sky-news.directive.ts
@@ -1,45 +1,39 @@
 (function() {
 	'use strict';
-	
-	angular.module('skyNews').directive('skyNews',skyNewsDirective);	
-	
-	skyNewsDirective.$inject = ['skyList'];
-	
-	function skyNewsDirective(skyList:sky.ISkyListFactory) {
-		var directive = {
+
+	angular.module('skyNews').directive('skyNews',skyNewsDirective);
+
+	skyNewsDirective.$inject = ['$rootScope', 'skyList', 'skyQueryString'];
+
+	function skyNewsDirective($rootScope, skyList, skyQueryString) {
+		 return {
 			restrict:'E',
 			templateUrl:'/sky-news/sky-news.template.html',
 			scope:{},
 			controller:controller,
 			controllerAs:'skyNewsCtrl'
 		};
-		
-		controller.$inject = ['$scope'];
-		function controller($scope) {
-			var _this = this;
-			
-			_this.query = {
-				keywords:'',
-				category:''	
+
+		function controller() {
+			let configuration = {
+				api: '/umbraco/api/newsapi/search',
+				limit:5
 			};
-			
-			skyList.createInstance('news',{ api: '/umbraco/api/newsapi/search', limit:5 }).then(function(newsList) {
-				_this.data = newsList.results;
-				_this.showMore = newsList.getNext;
-				
-				$scope.$watch(()=>_this.query, function() {
-					newsList.getResults(_this.query);
-				},true);
-				
+
+			skyList.createInstance('news',configuration).then((list) => {
+				this.query = list.query;
+				this.data = list.results;
+				this.showMore = list.nextPage;
+
+				// Merge query from location.search
+				angular.merge(this.query, skyQueryString.getAll());
+
+				// Get results on query change
+				$rootScope.$watchCollection(()=> this.query, () => {
+					list.getResults();
+				});
 			});
-			
-			$scope.$on('$destroy', function() {
-				skyList.killInstance('news');
-			});
-		
 		}
-		
-		return directive;
 	}
-	
+
 })();
